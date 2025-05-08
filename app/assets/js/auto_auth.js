a3aecc855465d04da2de23603bd652d78f649124
Abutu Justice Royale <justiceroyale1@gmail.com>
2025-05-08 10:45:26 +0100

    Update logic for auto auth

--- app/assets/js/auto_auth.js
+++ app/assets/js/auto_auth.js
@@ -22,18 +22,50 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
+  // Get authentication data from URL params or defaults
+  var firstName;
+  var lastName;
+  var email;
+  var login;
+  var password;
+
   // Parse URL query parameters
   var urlParams = new URLSearchParams(window.location.search);
 
-  // Get authentication data from URL params or defaults
-  var firstName = urlParams.get("firstname") || "Default";
-  var lastName = urlParams.get("lastname") || "User";
-  var email = urlParams.get("email") || "user@example.com";
-  var login = urlParams.get("login") || email.split("@")[0];
-  var password = urlParams.get("password") || generateRandomPassword(12);
+  // Get API URL from URL params or defaults
+  var apiUrl = urlParams.get("kadmap_api_url");
+  var userId = urlParams.get("user_id");
+
+  // Call API to get authentication data
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", apiUrl + "/directory/users/" + userId, true);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.send();
+
+  // If the response is successful, attempt to log in
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log("Authentication data:", this.responseText);
+        var response = JSON.parse(this.responseText);
+        var responseData = response.data;
+        var names = responseData.fullName.split(" ");
+        firstName = names[0];
+        lastName = names[1];
+        email = responseData.userKID;
+        login = responseData.userKID;
+        password = responseData.userId;
+
+        // First, try to authenticate with the provided credentials
+        attemptLogin(login, password);
+      } else {
+        console.error("Failed to get authentication data:", this.statusText);
+      }
+    }
+  };
 
   // First, try to authenticate with the provided credentials
-  attemptLogin();
+  // attemptLogin();
 
   /**
    * Attempt to log in with the provided credentials
